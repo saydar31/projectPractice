ea4162bfe437b3177e17c465fb2eb4d154cd6582
saydar31 <ajdarshaydullin@gmail.com>
2022-12-14 15:04:00 +0300

    add backend

---
+++ backend/src/main/java/ru/itis/cargocrm/CargocrmApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.cargocrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargocrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargocrmApplication.class, args);
+	}
+
+}
